7e5d3151320e4ab47e0334f5a3d86a18b5a6e3c0
harshpahwa <harshpahwa74@gmail.com>
2022-11-15 00:33:21 +0530

    fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000;
+const port = process.env.PORT || 8000;
 const cookieParser = require("cookie-parser")
 app.use(express.urlencoded())
 app.use(cookieParser())
