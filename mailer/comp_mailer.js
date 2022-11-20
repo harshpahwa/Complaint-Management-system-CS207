60fdf6c530d4f6ce697549edd2bcf9c46f0928df
harshpahwa <harshpahwa74@gmail.com>
2022-11-20 16:41:52 +0530

    harsh

--- mailer/comp_mailer.js
+++ mailer/comp_mailer.js
@@ -6,20 +6,28 @@ exports.newComp = (complaint) => {
     console.log('inside mailer');
     // let htmlString = nodeMailer.renderTemplate('mail.ejs');
     nodeMailer.transporter.sendMail({
-       from: 'b21100@students.iitmandi.ac.in',
-       to: 'harsh100pahwa@gmail.com',
-       subject: "New Complaint Published!",
-       //TODO
-        html:"<p>Your message "+complaint+".Message continueous </p>"
-    }, 
-    //info carries the info of the mail that has been sent
-    (err, info) => {
-        if (err){
-            console.log('Error in sending mail', err);
-            return;
-        }
+        from: 'b21100@students.iitmandi.ac.in',
+        to: 'harsh100pahwa@gmail.com',
+        subject: "New Complaint Published!",
+        //TODO
+        html: "<p>Complaint details <br>" + "<ul>"  
+        +"<li> Name: " + complaint.name + "</li>"
+        +"<li> Roll No: " + complaint.roll + "</li>"
+        +"<li> Room No: " + complaint.room + "</li>"
+        +"<li> Hostel:" + complaint.campus + " -> " + complaint.block  + " -> " + complaint.hostel + "</li>"
+        +"<li> Category: " + complaint.category + "</li>"
+        +"<li> Subject: " + complaint.subject + "</li>"
+        +"<li> Description: " + complaint.desc + "</li>"+
+        "</ul> Thank You</p>"
+    },
+        //info carries the info of the mail that has been sent
+        (err, info) => {
+            if (err) {
+                console.log('Error in sending mail', err);
+                return;
+            }
 
-        console.log('Message sent', info);
-        return;
-    });
+            console.log('Message sent', info);
+            return;
+        });
 }
